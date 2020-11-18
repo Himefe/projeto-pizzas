36afa932da7ffa8ee90814425d8369bbc5bd0912
Higor Almeida <idanger2015@hotmail.com>
2020-11-18 13:08:14 -0300

    Adicionando modal, modificando preço conforme o aumenta da unidade da pizza, etc

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 
 
 let modalQuantidade;
+let modalKey = 0;
+
 pizzaJson.map((item, index) => {
   const cloneModels = document.querySelector('.models .pizza-item').cloneNode(true);
   cloneModels.querySelector('.pizza-item--name').innerHTML = item.name;
-  cloneModels.setAttribute('data-key', index);
   cloneModels.querySelector('.pizza-item--price').innerHTML = `${item.price.toLocaleString("pt-br", {style: "currency" ,currency: "BRL" })}`;
   cloneModels.querySelector('.pizza-item--desc').innerHTML = item.description;
   cloneModels.querySelector('.pizza-item--img img').src = item.img;
+  cloneModels.setAttribute('data-key', index);
 
   document.querySelector('.pizza-area').append(cloneModels);
 
@@ -17,6 +19,9 @@ pizzaJson.map((item, index) => {
 
   const toClick = (event) => {
     modalQuantidade = 1;
+    let key = event.target.closest('.pizza-item').getAttribute('data-key');
+    modalKey = key;
+    console.log(item);
     event.preventDefault();
     const divModal = document.querySelector('.pizzaWindowArea');
     divModal.style.display = 'flex';
@@ -40,35 +45,55 @@ pizzaJson.map((item, index) => {
         size.classList.add('selected');
       }
       size.querySelector('span').innerHTML = item.sizes[sizeIndex];
+      
     });
-
+    
+    
     const pizzaInfoPrice = document.querySelector('.pizzaInfo--actualPrice');
     pizzaInfoPrice.innerText = item.price.toLocaleString("pt-br", {style: "currency" ,currency: "BRL" });
-  }
+
+}
+
 
 
 
   const clickerModal = cloneModels.querySelector('a');
   clickerModal.addEventListener('click', toClick);
-
-
-
 });
 
-const qntMais = (event) => {
+const pizzaInfoQt = document.querySelector('.pizzaInfo--qt');
+const pizzaInfoActualPrice = document.querySelector('.pizzaInfo--actualPrice');
+
+const qntMenos = (event) => {
   event.preventDefault();
-  modalQuantidade++;
-  const pizzaInfoQt = document.querySelector('.pizzaInfo--qt');
-  pizzaInfoQt.innerText = modalQuantidade;
-  const pizzaInfoPrice = document.querySelector('.pizzaInfo--actualPrice');
-    pizzaInfoPrice.innerText = (pizzaJson.price * modalQuantidade).toLocaleString("pt-br", {style: "currency" ,currency: "BRL" });
   
+  if (pizzaInfoQt.innerText > 1) {
+    modalQuantidade--;
+  } 
+  pizzaInfoQt.innerText = modalQuantidade;
+  pizzaInfoActualPrice.innerText = (pizzaJson[modalKey].price * modalQuantidade).toLocaleString("pt-br", {style: "currency" ,currency: "BRL" })
+
 }
 
+const qntMais = (event) => {
+  
+  if (pizzaInfoQt.innerText >= 1 && pizzaInfoQt.innerText < 10) {
+    modalQuantidade++;
+  }
+  event.preventDefault();
+  pizzaInfoQt.innerText = modalQuantidade;
+  pizzaInfoActualPrice.innerText = (pizzaJson[modalKey].price * modalQuantidade).toLocaleString("pt-br", {style: "currency" ,currency: "BRL" })
+
+  
+};
+
 const clickerQtmais = document.querySelector('.pizzaInfo--qtmais');
-clickerQtmais.addEventListener('click', qntMais);
+  clickerQtmais.addEventListener('click', qntMais);
 
-
+  const clickerQtMenos = document.querySelector('.pizzaInfo--qtmenos');
+  clickerQtMenos.addEventListener('click', qntMenos);
+  
+  
 
 const closeModal = () => {
   const divModal = document.querySelector('.pizzaWindowArea');
@@ -77,7 +102,10 @@ const closeModal = () => {
 
 }
 
+const buttonCard = () => {
+  console.log('PIZZA: '+modalKey)
+}
+
+document.querySelector('.pizzaInfo--addButton').addEventListener('click', buttonCard);
 document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', closeModal);
 document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', closeModal);
-
-
